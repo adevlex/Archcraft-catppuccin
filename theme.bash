5b8aafbabe913f8bb0b8a4649ae81cf74f849272
Alex Rosas <rosasrias82@gmail.com>
2024-12-24 21:35:58 -0500

    Catppuccin theme for Archcraft

---
+++ theme.bash
@@ -0,0 +1,92 @@
+# ------------------------------------------------------------------------------
+# Copyright (C) 2020-2024 Aditya Shakya <adi1090x@gmail.com>
+#
+# Gray Theme
+# ------------------------------------------------------------------------------
+
+# Colors
+background='#1E1D2D'
+foreground='#D9E0EE'
+color0='#1A1826'
+color1='#F28FAD'
+color2='#ABE9B3'
+color3='#F9E2AF'
+color4='#96CDFB'
+color5='#DDB6F2'
+color6='#89DCEB'
+color7='#D9E0EE'
+color8='#6E6A86'
+color9='#F28FAD'
+color10='#ABE9B3'
+color11='#F9E2AF'
+color12='#96CDFB'
+color13='#DDB6F2'
+color14='#89DCEB'
+color15='#F5E0DC'
+
+accent='#eba0ac'
+light_value='0.10'
+dark_value='0.50'
+
+# Wallpaper
+# wdir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+wallpaper="~/Pictures/wallpapers/mount.jpg"
+
+# Polybar
+polybar_font='Iosevka:style=bold:size=10;3'
+
+# Rofi
+rofi_font='Iosevka 10'
+rofi_icon='Vimix-White-Dark'
+
+# Terminal
+terminal_font_name='Iosevka'
+terminal_font_size='13.5'
+
+# Geany
+geany_colors='mountain.conf'
+geany_font='JetBrainsMono Nerd Font 10'
+
+# Appearance
+gtk_font='Noto Sans 9'
+gtk_theme='Catppuccin-Mocha'
+icon_theme='BeautySolar'
+cursor_theme='Bibata'
+
+# Openbox
+ob_theme='OB-Catppuccin-Mocha'
+ob_layout='DLIMC'
+ob_font='JetBrains Mono'
+ob_font_size='9'
+ob_menu='menu-icons.xml'
+ob_margin_t='80'
+ob_margin_b='20'
+ob_margin_l='20'
+ob_margin_r='20'
+
+# Dunst
+dunst_width='300'
+dunst_height='80'
+dunst_offset='40x100'
+dunst_origin='top-right'
+dunst_font='JetBrains Mono 10'
+dunst_border='0'
+dunst_separator='2'
+
+# Plank
+plank_hmode='auto'
+plank_offset='0'
+plank_position='bottom'
+plank_theme='Transparent'
+plank_icon_size='32'
+plank_zoom_percent='120'
+
+# Picom
+picom_backend='glx'
+picom_corner='10'
+picom_shadow_r='20'
+picom_shadow_o='0.60'
+picom_shadow_x='-20'
+picom_shadow_y='-20'
+picom_blur_method='none'
+picom_blur_strength='0'
